f25690239404dddd101a4b99f2beba3d53ac0624
adimunawar31 <munawar.adi31@gmail.com>
2022-12-23 15:38:55 +0700

    Initial Commit

---
+++ contracts/CrowdFunding'.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract MyContract {
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        string image;
+        uint256 amountCollected;
+        address[] donators;
+        uint256[] donations;
+    }
+
+    mapping(uint256 => Campaign) public campaigns;
+
+    uint256 public numberOfCampaigns = 0;
+
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        // is everything okey?
+        require(
+            campaign.deadline < block.timestamp,
+            "The deadline should be a date in the future"
+        );
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+    }
+
+    // function donateToCampaign() {}
+
+    // function getDonators() {}
+
+    // function GetCampaigns() {}
+}
